08133c216d0b54ed1b39af0a9cfa02766a9112ae
andyhebear@gmail.com <andyhebear@gmail.com@9067fe43-6e60-014a-d0ce-f990772a50cf>
2014-01-26 02:59:23 +0000

    icospheregenerator update

--- trunk/ProceduralIcoSphereGenerator.cs
+++ trunk/ProceduralIcoSphereGenerator.cs
@@ -26,7 +26,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 -----------------------------------------------------------------------------
 */
-
+//#ifndef PROCEDURAL_ICOSPHERE_GENERATOR_INCLUDED
+#define PROCEDURAL_ICOSPHERE_GENERATOR_INCLUDED
+//write with new std... ok
 namespace Mogre_Procedural
 {
     using System;
@@ -35,6 +37,7 @@ namespace Mogre_Procedural
 
     using Mogre;
     using Math = Mogre.Math;
+    using Mogre_Procedural.std;
     //*
     // * \ingroup objgengrp
     // * Builds an icosphere mesh, ie a sphere built with equally sized triangles
@@ -54,7 +57,7 @@ namespace Mogre_Procedural
         public IcoSphereGenerator()
             : this(1.0f, 2) {
         }
-        //C++ TO C# CONVERTER NOTE: Overloaded method(s) are created above to convert the following method having default parameters:
+       //
         //ORIGINAL LINE: IcoSphereGenerator(Ogre::float radius = 1.0f, uint numIterations = 2) : mRadius(radius), mNumIterations(numIterations)
         public IcoSphereGenerator(float radius, uint numIterations) {
             mRadius = radius;
@@ -68,40 +71,60 @@ namespace Mogre_Procedural
         //
         //ORIGINAL LINE: void addToTriangleBuffer(TriangleBuffer& buffer) const
         public override void addToTriangleBuffer(ref TriangleBuffer buffer) {
-            List<Vector3> vertices = new List<Vector3>();
+            std_vector<Vector3> vertices = new  std_vector<Vector3>();
             int offset = 0;
 
             /// Step 1 : Generate icosahedron
             float phi = 0.5f * (1.0f + Math.Sqrt(5.0f));
-            float invnorm = 1f / Math.Sqrt(phi * phi + 1);
+            float invnorm = 1f / Math.Sqrt(phi * phi + 1f);
 
-            vertices.Add(invnorm * new Vector3(-1, phi, 0)); //0
-            vertices.Add(invnorm * new Vector3(1, phi, 0)); //1
-            vertices.Add(invnorm * new Vector3(0, 1, -phi)); //2
-            vertices.Add(invnorm * new Vector3(0, 1, phi)); //3
-            vertices.Add(invnorm * new Vector3(-phi, 0, -1)); //4
-            vertices.Add(invnorm * new Vector3(-phi, 0, 1)); //5
-            vertices.Add(invnorm * new Vector3(phi, 0, -1)); //6
-            vertices.Add(invnorm * new Vector3(phi, 0, 1)); //7
-            vertices.Add(invnorm * new Vector3(0, -1, -phi)); //8
-            vertices.Add(invnorm * new Vector3(0, -1, phi)); //9
-            vertices.Add(invnorm * new Vector3(-1, -phi, 0)); //10
-            vertices.Add(invnorm * new Vector3(1, -phi, 0)); //11
+            vertices.push_back(invnorm * new Vector3(-1f, phi, 0f)); //0
+            vertices.push_back(invnorm * new Vector3(1f, phi, 0f)); //1
+            vertices.push_back(invnorm * new Vector3(0f, 1f, -phi)); //2
+            vertices.push_back(invnorm * new Vector3(0f, 1f, phi)); //3
+            vertices.push_back(invnorm * new Vector3(-phi, 0f, -1f)); //4
+            vertices.push_back(invnorm * new Vector3(-phi, 0f, 1f)); //5
+            vertices.push_back(invnorm * new Vector3(phi, 0f, -1f)); //6
+            vertices.push_back(invnorm * new Vector3(phi, 0f, 1f)); //7
+            vertices.push_back(invnorm * new Vector3(0f, -1f, -phi)); //8
+            vertices.push_back(invnorm * new Vector3(0f, -1f, phi)); //9
+            vertices.push_back(invnorm * new Vector3(-1f, -phi, 0f)); //10
+            vertices.push_back(invnorm * new Vector3(1f, -phi, 0f)); //11
 
-            int[] firstFaces = { 0, 1, 2, 0, 3, 1, 0, 4, 5, 1, 7, 6, 1, 6, 2, 1, 3, 7, 0, 2, 4, 0, 5, 3, 2, 6, 8, 2, 8, 4, 3, 5, 9, 3, 9, 7, 11, 6, 7, 10, 5, 4, 10, 4, 8, 10, 9, 5, 11, 8, 6, 11, 7, 9, 10, 8, 11, 10, 11, 9 };
+            int[] firstFaces = { 0, 1, 2,
+                                   0, 3, 1,
+                                   0, 4, 5, 
+                                   1, 7, 6, 
+                                   1, 6, 2, 
+                                   1, 3, 7,
+                                   0, 2, 4, 
+                                   0, 5, 3, 
+                                   2, 6, 8,
+                                   2, 8, 4, 
+                                   3, 5, 9, 
+                                   3, 9, 7,
+                                   11, 6, 7, 
+                                   10, 5, 4, 
+                                   10, 4, 8, 
+                                   10, 9, 5,
+                                   11, 8, 6,
+                                   11, 7, 9, 
+                                   10, 8, 11, 
+                                   10, 11, 9 
+                               };
 
             //C++ TO C# CONVERTER WARNING: This 'sizeof' ratio was replaced with a direct reference to the array length:
             //ORIGINAL LINE: std::vector<int> faces(firstFaces, firstFaces + sizeof(firstFaces)/sizeof(*firstFaces));
             // 定义一个容纳100个int型数据的容器,初值赋为0
             //vector<int> vecMyHouse(100,0);
-            List<int> faces = new List<int>(firstFaces);//(firstFaces, firstFaces + firstFaces.Length);
+            std_vector<int> faces = new  std_vector<int>(firstFaces);//(firstFaces, firstFaces + firstFaces.Length);
 
             int size = 60;
 
             /// Step 2 : tessellate
             for (ushort iteration = 0; iteration < mNumIterations; iteration++) {
                 size *= 4;
-                List<int> newFaces = new List<int>();
+                std_vector<int> newFaces = new std_vector<int>();
                 newFaces.Clear();
                 //newFaces.resize(size);
                 for (int i = 0; i < size / 12; i++) {
@@ -115,30 +138,30 @@ namespace Mogre_Procedural
                     Vector3 v2 = vertices[i2];
                     Vector3 v3 = vertices[i3];
                     //make 1 vertice at the center of each edge and project it onto the sphere
-                    vertices.Add((v1 + v2).NormalisedCopy);
-                    vertices.Add((v2 + v3).NormalisedCopy);
-                    vertices.Add((v1 + v3).NormalisedCopy);
+                    vertices.push_back((v1 + v2).NormalisedCopy);
+                    vertices.push_back((v2 + v3).NormalisedCopy);
+                    vertices.push_back((v1 + v3).NormalisedCopy);
                     //now recreate indices
-                    newFaces.Add(i1);
-                    newFaces.Add(i12);
-                    newFaces.Add(i13);
-                    newFaces.Add(i2);
-                    newFaces.Add(i23);
-                    newFaces.Add(i12);
-                    newFaces.Add(i3);
-                    newFaces.Add(i13);
-                    newFaces.Add(i23);
-                    newFaces.Add(i12);
-                    newFaces.Add(i23);
-                    newFaces.Add(i13);
+                    newFaces.push_back(i1);
+                    newFaces.push_back(i12);
+                    newFaces.push_back(i13);
+                    newFaces.push_back(i2);
+                    newFaces.push_back(i23);
+                    newFaces.push_back(i12);
+                    newFaces.push_back(i3);
+                    newFaces.push_back(i13);
+                    newFaces.push_back(i23);
+                    newFaces.push_back(i12);
+                    newFaces.push_back(i23);
+                    newFaces.push_back(i13);
                 }
                 //faces.swap(newFaces);
                 faces = newFaces;
             }
 
             /// Step 3 : generate texcoords
-            List<Vector2> texCoords = new List<Vector2>();
-            for (ushort i = 0; i < vertices.Count; i++) {
+            std_vector<Vector2> texCoords = new std_vector<Vector2>();
+            for (ushort i = 0; i < vertices.size(); i++) {
                 Vector3 vec = vertices[i];
                 float u = 0f;
                 float v = 0f;
@@ -147,52 +170,52 @@ namespace Mogre_Procedural
                 alpha = atan2f(vec.z, vec.x);
                 u = alpha / Math.TWO_PI + .5f;
                 v = atan2f(vec.y, r0) / Math.PI + .5f;
-                texCoords.Add(new Vector2(u, v));
+                texCoords.push_back(new Vector2(u, v));
             }
 
             /// Step 4 : fix texcoords
             // find vertices to split
-            List<int> indexToSplit = new List<int>();
+            std_vector<int> indexToSplit = new  std_vector<int>();
 
-            for (int i = 0; i < faces.Count / 3; i++) {
+            for (int i = 0; i < faces.size() / 3; i++) {
                 Vector2 t0 = texCoords[faces[i * 3 + 0]];
                 Vector2 t1 = texCoords[faces[i * 3 + 1]];
                 Vector2 t2 = texCoords[faces[i * 3 + 2]];
                 if (Math.Abs(t2.x - t0.x) > 0.5) {
                     if (t0.x < 0.5)
-                        indexToSplit.Add(faces[i * 3]);
+                        indexToSplit.push_back(faces[i * 3]);
                     else
-                        indexToSplit.Add(faces[i * 3 + 2]);
+                        indexToSplit.push_back(faces[i * 3 + 2]);
                 }
                 if (Math.Abs(t1.x - t0.x) > 0.5) {
                     if (t0.x < 0.5)
-                        indexToSplit.Add(faces[i * 3]);
+                        indexToSplit.push_back(faces[i * 3]);
                     else
-                        indexToSplit.Add(faces[i * 3 + 1]);
+                        indexToSplit.push_back(faces[i * 3 + 1]);
                 }
                 if (Math.Abs(t2.x - t1.x) > 0.5) {
                     if (t1.x < 0.5)
-                        indexToSplit.Add(faces[i * 3 + 1]);
+                        indexToSplit.push_back(faces[i * 3 + 1]);
                     else
-                        indexToSplit.Add(faces[i * 3 + 2]);
+                        indexToSplit.push_back(faces[i * 3 + 2]);
                 }
             }
 
             //split vertices
-            for (ushort i = 0; i < indexToSplit.Count; i++) {
+            for (ushort i = 0; i < indexToSplit.size(); i++) {
                 int index = indexToSplit[i];
                 //duplicate vertex
                 Vector3 v = vertices[index];
                 Vector2 t = texCoords[index] + Vector2.UNIT_X;
-                vertices.Add(v);
-                texCoords.Add(t);
-                int newIndex = vertices.Count - 1;
+                vertices.push_back(v);
+                texCoords.push_back(t);
+                int newIndex = vertices.size() - 1;
                 //reassign indices
-                for (ushort j = 0; j < faces.Count; j++) {
+                for (ushort j = 0; j < faces.size(); j++) {
                     if (faces[j] == index) {
                         int index1 = faces[(j + 1) % 3 + (j / 3) * 3];
                         int index2 = faces[(j + 2) % 3 + (j / 3) * 3];
-                        if ((texCoords[index1].x > 0.5) || (texCoords[index2].x > 0.5)) {
+                        if ((texCoords[index1].x > 0.5f) || (texCoords[index2].x > 0.5f)) {
                             faces[j] = newIndex;
                         }
                     }
@@ -201,16 +224,16 @@ namespace Mogre_Procedural
 
             /// Step 5 : realize
             buffer.rebaseOffset();
-            buffer.estimateVertexCount((uint)vertices.Count);
+            buffer.estimateVertexCount((uint)vertices.size());
             buffer.estimateIndexCount((uint)size);
 
-            for (ushort i = 0; i < vertices.Count; i++) {
+            for (ushort i = 0; i < vertices.size(); i++) {
                 addPoint(ref buffer, mRadius * vertices[i], vertices[i], new Vector2(texCoords[i].x, texCoords[i].y));
             }
             for (ushort i = 0; i < size; i++) {
                 buffer.index(offset + faces[i]);
             }
-            offset += vertices.Count;
+            offset += vertices.size();
         }
 
 
@@ -221,11 +244,7 @@ namespace Mogre_Procedural
         //	
         public IcoSphereGenerator setRadius(float radius) {
             if (radius <= 0.0f)
-                //C++ TO C# CONVERTER TODO TASK: There is no direct equivalent in C# to the C++ __LINE__ macro:
-                //C++ TO C# CONVERTER TODO TASK: There is no direct equivalent in C# to the C++ __FILE__ macro:
-                //throw ExceptionFactory.create(Mogre.ExceptionCodeType<Mogre.Exception.ExceptionCodes.ERR_INVALIDPARAMS>(), "Radius must be larger than 0!", "Procedural::IcoSphereGenerator::setRadius(Ogre::Real)", __FILE__, __LINE__);
-                throw new Exception("Radius must be larger than 0!");
-            ;
+             OGRE_EXCEPT("Ogre::Exception::ERR_INVALIDPARAMS", "Radius must be larger than 0!", "Procedural::IcoSphereGenerator::setRadius(Ogre::Real)");            ;
             mRadius = radius;
             return this;
         }
@@ -238,10 +257,7 @@ namespace Mogre_Procedural
         //	
         public IcoSphereGenerator setNumIterations(uint numIterations) {
             if (numIterations == 0)
-                //C++ TO C# CONVERTER TODO TASK: There is no direct equivalent in C# to the C++ __LINE__ macro:
-                //C++ TO C# CONVERTER TODO TASK: There is no direct equivalent in C# to the C++ __FILE__ macro:
-                //throw ExceptionFactory.create(Mogre.ExceptionCodeType<Mogre.Exception.ExceptionCodes.ERR_INVALIDPARAMS>(), "There must be more than 0 iterations", "Procedural::IcoSphereGenerator::setNumRings(unsigned int)", __FILE__, __LINE__);
-                throw new Exception("numIterations must be larger than 0!");
+             OGRE_EXCEPT("Ogre::Exception::ERR_INVALIDPARAMS", "There must be more than 0 iterations", "Procedural::IcoSphereGenerator::setNumRings(unsigned int)");
             ;
             mNumIterations = numIterations;
             return this;
